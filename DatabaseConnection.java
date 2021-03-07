df3a4fac460c971066a41a81d6a4f1901e5f6beb
Bora Çün <cun2424@gmail.com>
2021-03-07 22:30:22 +0300

    Table populations complete

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -74,8 +74,138 @@ public class DatabaseConnection
                 "FOREIGN KEY (cid) REFERENCES company(cid)) " +
                 "ENGINE=InnoDB;");
             System.out.println("Created apply table.");
-            
+
             System.out.println(divider);
+
+            //Populating student table
+            //John
+            stmt.executeUpdate("INSERT INTO student VALUES(" +
+                "'21000001', " + 
+                "'John', " +
+                "'1999-05-14', " +
+                "'Windy', " + 
+                "'Chicago', " + 
+                "'senior', " + 
+                "2.33, " + 
+                "'US'); ");
+
+            //Ali
+            stmt.executeUpdate("INSERT INTO student VALUES(" +
+                "'21000002', " + 
+                "'Ali', " +
+                "'2001-09-30', " +
+                "'Nisantasi', " + 
+                "'Istanbul', " + 
+                "'junior', " + 
+                "3.26, " + 
+                "'TC'); ");
+
+            //Veli
+            stmt.executeUpdate("INSERT INTO student VALUES(" +
+                "'21000003', " + 
+                "'Veli', " +
+                "'2003-02-25', " +
+                "'Nisantasi', " + 
+                "'Istanbul', " + 
+                "'freshman', " + 
+                "2.41, " + 
+                "'TC'); ");
+
+            //Ayse
+            stmt.executeUpdate("INSERT INTO student VALUES(" +
+                "'21000004', " + 
+                "'Ayse', " +
+                "'2003-01-15', " +
+                "'Tunali', " + 
+                "'Ankara', " + 
+                "'freshman', " + 
+                "2.55, " + 
+                "'TC'); ");
+            System.out.println("student population complete.");
+
+            //Populating company table
+            //Microsoft
+            stmt.executeUpdate("INSERT INTO company VALUES(" +
+                "'C101', " + 
+                "'microsoft', " +
+                "2); ");
+
+            //Merkez bankasi
+            stmt.executeUpdate("INSERT INTO company VALUES(" +
+                "'C102', " + 
+                "'merkez bankasi', " +
+                "5); ");
+            
+            //tai
+            stmt.executeUpdate("INSERT INTO company VALUES(" +
+                "'C103', " + 
+                "'tai', " +
+                "3); ");
+
+            //tubitak
+            stmt.executeUpdate("INSERT INTO company VALUES(" +
+                "'C104', " + 
+                "'tubitak', " +
+                "5); ");
+
+            //aselsan
+            stmt.executeUpdate("INSERT INTO company VALUES(" +
+                "'C105', " + 
+                "'aselsan', " +
+                "3); ");
+
+            //havelsan
+            stmt.executeUpdate("INSERT INTO company VALUES(" +
+                "'C106', " + 
+                "'havelsan', " +
+                "4); ");
+
+            //milsoft
+            stmt.executeUpdate("INSERT INTO company VALUES(" +
+                "'C107', " + 
+                "'milsoft', " +
+                "2); ");
+            System.out.println("company population complete.");
+
+            //Populating apply table
+            //1
+            stmt.executeUpdate("INSERT INTO apply VALUES(" +
+                "'21000001', " + 
+                "'C101'); ");
+
+            stmt.executeUpdate("INSERT INTO apply VALUES(" +
+                "'21000001', " + 
+                "'C102'); ");
+
+            stmt.executeUpdate("INSERT INTO apply VALUES(" +
+                "'21000001', " + 
+                "'C103'); ");
+
+            //2
+            stmt.executeUpdate("INSERT INTO apply VALUES(" +
+                "'21000002', " + 
+                "'C101'); ");
+            
+            stmt.executeUpdate("INSERT INTO apply VALUES(" +
+                "'21000002', " + 
+                "'C105'); ");
+
+            //3
+            stmt.executeUpdate("INSERT INTO apply VALUES(" +
+                "'21000003', " + 
+                "'C104'); ");
+
+            stmt.executeUpdate("INSERT INTO apply VALUES(" +
+                "'21000003', " + 
+                "'C105'); ");
+
+            //4
+            stmt.executeUpdate("INSERT INTO apply VALUES(" +
+                "'21000004', " + 
+                "'C107'); ");
+            
+            
+            System.out.println("apply population complete.");
 
             ResultSet rs = stmt.executeQuery("select * from student");
 
