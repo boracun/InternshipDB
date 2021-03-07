2640a51cae5f040b0c9e1b8cdd43ce59417a2e09
Bora Çün <cun2424@gmail.com>
2021-03-07 21:39:37 +0300

    Connection error messages added

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -7,21 +7,42 @@ public class DatabaseConnection
 {
     public static void main(String [] args)
     {
+        //Constants
+        final String url = "jdbc:mysql://dijkstra.ug.bcc.bilkent.edu.tr:3306/bora_cun";
+        final String user = "bora.cun";
+        final String pass = "DPZ3a7Km";
+
+        //Variables
+        String errorMessage;
+
+        //Program
+        
         try
         {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://dijkstra.ug.bcc.bilkent.edu.tr:3306/bora_cun", "bora.cun", "DPZ3a7Km");
+
+            //Connection
+            System.out.println("Connecting to the database...");
+            Connection con = DriverManager.getConnection(url, user, pass);
+            System.out.println("Connection successful...");
 
             Statement stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery("select * from emp");
+            ResultSet rs = stmt.executeQuery("select * from student");
 
             while(rs.next())
-                System.out.println(rs.getInt(1) + " " + rs.getString(2) + " " + rs.getString(3));
+                System.out.println(rs.getInt("sid") + " " + rs.getString("sname") + " " + rs.getString("bdate"));
             con.close();
         }
         catch(Exception e)
         {
-            System.out.println(e);
+            errorMessage = e.toString();
+            if (errorMessage.contains("Access denied"))
+                System.out.println("Wrong login information.");
+            else if (errorMessage.contains("driver found"))
+                System.out.println("Wrong URL.");
         }
+        
+
+        
     }
 }
